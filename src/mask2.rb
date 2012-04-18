1694c433f710b7f8d9e9ae9b378098cd90c4a382
Katarzyna Rycerz <kzajac@agh.edu.pl>
2012-04-18 07:02:26 -0700

    prototype of new DSL

--- src/mask2.rb
+++ src/mask2.rb
@@ -1,4 +1,12 @@
 # To change this template, choose Tools | Templates
 # and open the template in the editor.
 
+
+class DSLElement
+ def copyvars
+  self.class.instance_variables.each do |var|
+   instance_variable_set(var, self.class.instance_variable_get(var))
+  end 
+ end
+end
 puts "Hello World"
