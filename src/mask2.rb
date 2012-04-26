71865752117f1589a6e3b4b81b33ea9f2953b06f
Katarzyna Rycerz <kzajac@agh.edu.pl>
2012-04-26 11:48:44 +0200

    working prototype of new DSL for generating erlang processes

--- src/mask2.rb
+++ src/mask2.rb
@@ -17,6 +17,8 @@ class Modules <DSLElement
       f.copyvars
       return f
     end
+
+
     def self.app_module (name, &blk)
       @modules||=Hash.new
       klass = Class.new(Mod)
@@ -29,38 +31,48 @@ class Modules <DSLElement
       p.class.class_eval(&blk) if block_given?
       p.copyvars
       @modules[name] = p
-
     end
+
+
     def self.execute (name)
       print "-module(#{@my_name.to_s}).
 -export(["
       number_modules=@modules.length
       number_cur=0;
       @modules.each_key { |key|
-          print "loop#{key.to_s.capitalize}/1"
+          rep=@modules[key].execution.rep
+          if rep<2
+            args=0
+          else
+            args=1
+          end
+          print "loop#{key.to_s.capitalize}/#{args}"
           number_cur=number_cur+1
           if (number_cur<number_modules)
             print ","
           end
       }
-      puts "])\n"
+      puts "]).\n"
       @modules.each_key { |key|
-          p "%erlang code generated for module #{key}"
+          puts "%erlang code generated for module #{key}"
           @modules[key].execute
       }
     end
  end
 
- class Element < DSLElement
- attr_accessor :name
 
- def initialize(name=nil)
-  @name = name
+
+ class Element < DSLElement
+  attr_accessor :name
+
+  def initialize(name=nil)
+    @name = name
+  end
  end
-end
 
 class Mod < Element
- attr_accessor :subname
+  attr_accessor :subname, :execution
+
   def initialize(name=nil)
     @name = name
     @subname=nil
@@ -74,70 +86,64 @@ class Mod < Element
   def self.execution (rep=1, &blk)
       klass = Class.new(Execution)
       name_s="Exec"+self.name.to_s
-      #puts "Nazwa " + name_s
       Object.const_set(name_s, klass)  unless Object.const_defined?(name_s)
       p = Object.const_get(name_s).new
       p.class.class_eval(&blk) if block_given?
       p.copyvars
       @execution = p
-  #    @execution.commends.push(:k=>:k)
       @execution.rep=rep
   end
 
   def execute
-    #puts @name
-   # puts @cores
-   # puts @execution
-   name_s="loop"+@name.to_s
-   if (@execution.rep>=2)
-   puts "
-
+    name_s="loop"+@name.to_s
+    if (@execution.rep>=2)
+           puts "
 #{name_s}(0) ->
        io:format(\"~w ending ~n\",[self()]);
 
 #{name_s}(Number) ->
-
-
     "
-  else
-    puts "
+    else
+      puts "
 #{name_s}() ->
       "
-  end
-   number_of_commends=@execution.commends.length
-      number_cur=0;
+    end
+    number_of_commends=@execution.commends.length
+    number_cur=0;
     @execution.commends.each {|commend|
 
-
-     
       commend.each_key do |commend_symbol|
 
          #puts commend_symbol
          if commend_symbol==:c
            print "io:format(\"~w calculating~n\",[self()]),\ntimer:sleep(#{commend[commend_symbol]})"
          end
+
          if commend_symbol==:r
            pid_name=commend[commend_symbol].to_s.capitalize
-           print "io:format(\"~w waiting from answer from ~w ~n\",[self(), #{pid_name}]),\nreceive \n {#{pid_name}, Msg} -> \n\tio:format(\"~w received ~w~n\",[self(),Msg])\nend"
+           print "io:format(\"~w waiting for message ~n\",[self()]),\nreceive \n {#{pid_name}, Msg} -> \n\tio:format(\"~w received ~w~n\",[self(),Msg])\nend"
+         end
+
+         if commend_symbol==:e
+           pid_name=commend[commend_symbol].to_s.capitalize
+           print "io:format(\"~w sending message to ~w~n\",[self(),#{pid_name}]),\n#{pid_name} ! {self(), Msg}"
 
          end
+
          if commend_symbol==:s
              pid_name=commend[commend_symbol][:id].to_s.capitalize
-             module_name="loop"+commend[commend_symbol][:module].to_s.capitalize
+             module_name="loop"+commend[commend_symbol][:id].to_s.capitalize
 
              print "io:format(\"~w spawning ~n\",[self()]), 
 #{pid_name} = spawn(#{self.subname}, #{module_name}, []), \n
-io:format(\"~w sending data to ~w ~n\",[self(), Pid2]),
+io:format(\"~w sending data to ~w ~n\",[self(), #{pid_name}]),
 #{pid_name} ! {self(), value}"
-#puts commend[commend_symbol][:module].to_s.capitalize
-          # commend[commend_symbol].each_key do |key|
-            # puts "#{key} for #{commend[commend_symbol][key]}"
-          # end
-         #else
-          # puts commend[commend_symbol]
          end
          number_cur=number_cur+1
-         
+
+
+
+
          if (number_cur==number_of_commends)
            if (@execution.rep>=2)
             print ",\n#{name_s}(Number-1)."
@@ -149,26 +155,51 @@ io:format(\"~w sending data to ~w ~n\",[self(), Pid2]),
          end
          puts"\n"
      end
-     # puts "#{value[:s]}"
-
-
      }
   end
 end
+
+
 class Execution <DSLElement
   attr_accessor :commends, :rep
+
   def self.calculate (nb)
     @commends||=[]
     @commends.push(:c=>nb)
   end
+
   def self.spawn (sp_hash)
     @commends||=[]
     @commends.push(:s=>sp_hash)
+    
+    idik=sp_hash[:id].to_s.capitalize
+    core_number=sp_hash[:cores]
+    calculations_number=sp_hash[:calculate]
+    
+    Modules.app_module idik do
+        cores core_number
+
+        execution 1 do
+
+                   receive idik
+
+                   calculate calculations_number
+
+                   send  idik
+          end
+    end
   end
+
   def self.receive(id)
      @commends||=[]
-    @commends.push(:r=>id)
+     @commends.push(:r=>id)
   end
+
+  def self.send(id)
+     @commends||=[]
+     @commends.push(:e=>id)
+  end
+
 end
 #puts "Hello World"
 #if ARGV.size > 0
