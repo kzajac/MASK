79a642256b1903607ccd98863cb9cc56090ebf32
Katarzyna Rycerz <kzajac@agh.edu.pl>
2012-05-22 15:38:36 +0200

    beginning of cloud executor for the skeleton

---
+++ src/executor.rb
@@ -0,0 +1,28 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+class Calculating_object
+  def calculate (in_data, out_data, iter_number)
+    out_data=in_data+1
+  end
+end
+
+class Executor_Scenario
+  def self.calculate
+
+  end
+  def self.spawn
+
+  end
+  def self.receive
+
+  end
+  def self.send
+    
+  end
+
+end
+puts "Hello World"
+
+calc=Calculating_object.new
+puts calc.calculate(5, 6, 1)
+
