700ea798e3af4326ea6f12df845d2fd9210f10d0
Katarzyna Rycerz <kzajac@agh.edu.pl>
2013-02-06 16:06:47 +0100

    sin

--- src/reg_cli.rb
+++ src/reg_cli.rb
@@ -1,34 +1,24 @@
 require 'rubygems'
-    require "rest_client"
-    require "json"
-   # require 'uri'
+require "rest_client"
+require "json"
 
-    #1: Simple POST
-   # res = Net::HTTP.post_form(URI.parse('http://gs2.mapper-project.eu:1234/add_base/Submodel'),
-     #{"id"=>"92343", "name"=>'MojModel', "timescale[delta]"=>"10","timescale[max]"=>"100"})
-jdata = {:key => 'I am a value'}.to_json
-#jdata="Kasia".to_json
-puts jdata
-res= RestClient.post 'http://localhost:4567/calculations', {:data => jdata}, {:content_type => :json, :accept => :json}
-#res= RestClient.post 'http://localhost:4567/calculations', "message"=>"Kasia2"
-p res
+for iter in 1..10 do
+
+  jdata = {"name" =>"LU", "id"=>"1", "predcessor"=>"none", "iteration"=>iter}.to_json
+
+  puts jdata
+
+  res= RestClient.post 'http://localhost:4567/calculations', {:data => jdata}, {:content_type => :json, :accept => :json}
+
+  p res
+end
 
 res= RestClient.get 'http://localhost:4567/calculations/'
+
 p res
 
-#conn = Net::HTTP.new('http://gs2.mapper-project.eu/',1234)
-#request = Net::HTTP::Post.new("/add_base/Submodel")
-#request.set_form_data({"id"=>"923433", "name"=>'MojModel', "class"=>"my_class", "timescale[delta]"=>"10","timescale[max]"=>"100",
-    #   "spacescales[][delta]"=>["20"],"spacescales[][max]"=>"200" })
-#res = conn.request(request)
-   #res = Net::HTTP.post_form(URI.parse('http://gs2.mapper-project.eu:1234/add_base/Submodel'),
-    # {"id"=>"923433", "name"=>'MojModel', "class"=>"my_class", "timescale[delta]"=>"10","timescale[max]"=>"100",
-     # "spacescales[][delta]"=>"500", "spacescales[][max]"=>["cos1","cos2"],"scalespaces[][delta]"=>["99","88"] })
- #res = Net::HTTP.post_form(URI.parse('http://gs2.mapper-project.eu:1234/add_base/Submodel'),
-    # "id=923433&name=MojModel&class=my_class&timescale[delta]=10&timescale[max]=100")
-     #  "spacescales[][delta]"=>"20","spacescales[][max]"=>"300","spacescales[][delta]"=>"30","spacescales[][max]"=>"200" })
-  # res = Net::HTTP.post_form(URI.parse('http://gs2.mapper-project.eu:1234/add_base/Submodel'),
-    # {"id"=>"9234", "class"=>'jModel2'})
+#tablica=JSON.parse(res)
+#puts tablica
+
  
-    puts res
     
--- src/sinatra_try.rb
+++ src/sinatra_try.rb
@@ -11,28 +11,94 @@ require 'sinatra'
 require 'json'
 class Calculations
 
-  def self.add element
-   @calculations||=[]
-   @calculations.push(element)
+  def self.init_state_rout
+     @state1=0
+   end
+
+  def self.calculations_rout
+    @state1=@state1+1
+    puts @state1
+    beginning = Time.now
+    a = DMatrix.rand(1600, 1600)
+    l, u = a.lu
+    File.open("/home/kzajac/MASK/src/wyniki", 'a') {|f| f.write("Time elapsed #{Time.now - beginning} seconds\n")}
+    puts  "Time elapsed #{Time.now - beginning} seconds\n"
+    "wynik_obliczen"
   end
-  def self.calculations
-    @calculations
+
+  def self.add id, name, predcessor, iteration
+   @calculations||=Hash.new
+   @calculations_stack||=[]
+   identifier="#{id}_#{iteration}"
+   @calculations_stack.push(identifier)
+   @calculations[identifier]={"name"=>name, "predcessor"=>predcessor, "status"=> "waiting"}
+
   end
- end
+  def self.status_calc identifier
+    @calculations[identifier]["status"]
+  end
+  def self.all_statuses
+    return @calculations
+
+  end
+  def self.run_calc
+    puts "tutaj 2"
+    identifier=@calculations_stack.pop unless @calculations_stack.nil?
+    put identifier
+    @calculations[identifier ]["status"]="running"
+    calculations_rout unless identifier.nil?
+    @calculations[identifier]["status"]="finished"
+  end
+
+ 
+
+  
+
+end
+
+
+Thread.new do
+  Calculations.init_state_rout
+  while true do
+    puts"tutaj"
+    puts Calculations.all_statuses
+      Calculations.run_calc
+      sleep 1
+ 
+  end
+end
+
 post '/calculations' do
-      #"You said '#{params[:message]}'"
+     
       jdata = params[:data]
       for_json = JSON.parse(jdata)
-      Calculations.add(for_json)
-      #@calculations.push(:message)
-      "hello"
+
+      name= for_json["name"]
+      id= for_json["id"]
+      predcessor=for_json["predcessor"]
+      iteration=for_json["iteration"]
+
+      Calculations.add(id, name, predcessor, iteration)
+     
+      body("http://#{request.host}:#{request.port}/calculations/#{id}_#{iteration}")
+
+      status 200
     end
-
-
 get '/calculations/' do
- puts Calculations.calculations
- "kk"
+
+  body(Calculations.all_statuses.to_json)
+   status 200
 end
-get '/resources2/time' do
-  "Hello world, it's #{Time.now} at the server!"
+get '/calculations/:identifier' do
+  body(JSON.pretty_generate({:status=>Calculations.status_calc[params[identifier]]}))
+  status 200
+
+  #if Calculations.calculations[params[:number].to_i].nil?
+  #  body({:status=>:deleted}.to_json)
+  #  status 200
+ # else
+  #  puts params[:number].to_i
+  #  body({:status=>:running, :vales=>Calculations.calculations[params[:number].to_i].to_json}.to_json)
+   # status 200
+ #end
 end
